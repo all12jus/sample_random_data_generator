0dd590f9b26802da9ad10ec1cb21bb621c38a956
Justin Allen <all12jus@gmail.com>
2021-12-04 20:15:58 -0500

    Phase 2: Complete

--- main.js
+++ main.js
@@ -3,11 +3,11 @@
 
     output.GenerateSample = function(input_list, input_count){
         if (!Array.isArray(input_list)){
-            return null;
+            throw "First Argument is Not An Array.";
         }
 
         if (!Number.isInteger(input_count)){
-            return null;
+            throw "Second Argument is Not An Number.";
         }
 
         let output_array = [];
@@ -16,11 +16,37 @@
             input_list.forEach((input_item) => {
                 let key = input_item.key;
                 let value = input_item.type(i);
+                if (typeof value === "function"){
+                    throw "Type of entity can't be a function.";
+                }
                 item[key] = value;
             });
             output_array.push(item);
         }
         return output_array;
+    };
+
+    output.RowNumber = function (index) {
+        if (!Number.isInteger(index)){
+            return null;
+        }
+        return index;
+    };
+
+    output.RandomNumber = function (min, max) {
+        if (!Number.isInteger(min)){
+            throw "First Argument is Not An Number.";
+        }
+
+        if (!Number.isInteger(max)){
+            throw "Second Argument is Not An Number.";
+        }
+
+        if (min >= max){
+            throw "Second Argument is Greater Than First Argument.";
+        }
+
+        return Math.floor(Math.random() * (max - min + 1) + min)
     };
 
     module.exports = output;
--- main.test.js
+++ main.test.js
@@ -1,17 +1,25 @@
-const { GenerateSample } = require("./main");
+const { GenerateSample, RowNumber, RandomNumber } = require("./main");
 
 test('function exists', () => {
     expect(GenerateSample).not.toBeUndefined();
 });
 
 test('if first argument is not a array error', () => {
-    let sample = GenerateSample();
-    expect(sample).toBeNull();
+    expect(() => {
+        let sample = GenerateSample();
+    }).toThrow();
+    expect(() => {
+        let sample = GenerateSample();
+    }).toThrowError(/^First Argument is Not An Array.$/);
 });
 
 test('if second argument is not a number error', () => {
-    let sample = GenerateSample([]);
-    expect(sample).toBeNull();
+    expect(() => {
+        let sample = GenerateSample([]);
+    }).toThrow();
+    expect(() => {
+        let sample = GenerateSample([]);
+    }).toThrowError(/^Second Argument is Not An Number.$/);
 });
 
 test('([], 10) => [].length=10 ', () => {
@@ -39,4 +47,121 @@ test('([{digit}], 10) => [{digit}].length=10 ', () => {
     expect(Object.keys(sample[0]).length).toBe(1);
     expect(sample[0].digit).not.toBeNull();
     expect(sample[0].digit).toEqual(expect.any(Number));
+});
+
+test('RowNumber is a function', () => {
+    expect(RowNumber).not.toBeUndefined();
+    expect(RowNumber).not.toBeNull();
+    expect(RowNumber).toEqual(expect.any(Function));
+});
+
+test('RowNumber is a function (value:!Number) => null', () => {
+    let result = RowNumber('c');
+    expect(result).not.toBeUndefined();
+    expect(result).toBeNull();
+});
+
+test('RowNumber is a function (value:Number) => value', () => {
+    let result = RowNumber(100);
+
+    expect(result).not.toBeUndefined();
+    expect(result).not.toBeNull();
+    expect(result).toEqual(expect.any(Number));
+    expect(result).toEqual(100);
+});
+
+test('([{id:RowNumber}], 10) throws "Type of entity can\'t be a function."', () => {
+    expect(() => {
+        let sample = GenerateSample([
+            { key: "id", type: (index) => RowNumber }
+        ], 10);
+    }).toThrow();
+    expect(() => {
+        let sample = GenerateSample([
+            { key: "id", type: (index) => RowNumber }
+        ], 10);
+    }).toThrowError(/^Type of entity can't be a function.$/);
+});
+
+test('([{digit:fn], 10) [fn has been called 10 times] ', () => {
+    let fun = jest.fn();
+    let sample = GenerateSample([
+        {
+            key: "digit",
+            type: (index) => { fun(); return index; }
+        }
+    ], 10);
+    expect(fun).toHaveBeenCalledTimes(10);
+});
+
+test('([{id:RowNumber()}], 10) => [{id:Number}].length=10', () => {
+    let sample = GenerateSample([
+        { key: "id", type: (index) => RowNumber(index) }
+    ], 10);
+    expect(sample.length).toBe(10);
+    expect(sample[0]).not.toBeNull();
+    expect(Object.keys(sample[0]).length).toBe(1);
+    expect(sample[0].id).not.toBeNull();
+    expect(sample[0].id).toBe(0);
+    for (let i = 0; i < 10; i++) {
+        expect(sample[i].id).toBe(i);
+    }
+});
+
+describe('RandomNumber', function () {
+
+    test('RandomNumber is a function', () => {
+        expect(RandomNumber).not.toBeUndefined();
+        expect(RandomNumber).not.toBeNull();
+        expect(RandomNumber).toEqual(expect.any(Function));
+    });
+
+    test('RandomNumber is a function (min:!Number, _) throws', () => {
+        expect(() => {
+            let result = RandomNumber('c', 1);
+        }).toThrowError(/^First Argument is Not An Number.$/);
+    });
+
+    test('RandomNumber is a function (min:Number, max: !Number) throws', () => {
+        expect(() => {
+            let result = RandomNumber(1, 'c');
+        }).toThrowError(/^Second Argument is Not An Number.$/);
+    });
+
+    test('RandomNumber is a function (min:Number, max: Number) min > max throws', () => {
+        expect(() => {
+            let result = RandomNumber(10, 5);
+        }).toThrowError(/^Second Argument is Greater Than First Argument.$/);
+    });
+
+    test('RandomNumber is a function (min:Number, max: Number) min = max throws', () => {
+        expect(() => {
+            let result = RandomNumber(5, 5);
+        }).toThrowError(/^Second Argument is Greater Than First Argument.$/);
+    });
+
+    test('RandomNumber is a function (min:Number, max: Number) => value:Number between (inclusive)min and max', () => {
+        let result = RandomNumber(0, 10);
+
+        expect(result).not.toBeUndefined();
+        expect(result).not.toBeNull();
+        expect(result).toEqual(expect.any(Number));
+        expect(result).toBeGreaterThanOrEqual(0);
+        expect(result).toBeLessThan(10);
+    });
+
+    test('([{digit:RandomNumber(0,20}], 10) => [{digit:Number}].length=10 ', () => {
+        let sample = GenerateSample([
+            {
+                key: "digit",
+                type: (index) => RandomNumber(0, 20)
+            }
+        ], 10);
+        expect(sample.length).toBe(10);
+        expect(sample[0]).not.toBeNull();
+        expect(Object.keys(sample[0]).length).toBe(1);
+        expect(sample[0].digit).not.toBeNull();
+        expect(sample[0].digit).toEqual(expect.any(Number));
+    });
+    
 });
