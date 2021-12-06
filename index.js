aea3096db2103278ca015bc0784dd1573dc5c71f
Justin Allen <all12jus@gmail.com>
2021-12-06 17:57:58 -0500

    RandomDate implimented

--- index.js
+++ index.js
@@ -74,5 +74,27 @@
         return expression.gen();
     }
 
+    output.RandomEmail = function () {
+        const email_regex = new RandExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/);
+        return email_regex.gen();
+    }
+
+    output.RandomDate = function (start, end) {
+
+        if (start === undefined || start === null || start instanceof Date !== true){
+            throw "First Argument is Not An Date.";
+        }
+
+        if (end === undefined || end === null || end instanceof Date !== true){
+            throw "Second Argument is Not An Date.";
+        }
+
+        if (start.getTime() >= end.getTime()){
+            throw "Second Argument is Greater Than First Argument.";
+        }
+
+        return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    }
+
     module.exports = output;
 })()
--- index.test.js
+++ index.test.js
@@ -1,4 +1,7 @@
-const { GenerateSample, RowNumber, RandomNumber, RandomYear, RandomRegEx } = require("./index");
+const {
+    GenerateSample, RowNumber, RandomNumber, RandomYear, RandomRegEx,
+    RandomEmail, RandomDate
+} = require("./index");
 const RandExp = require('randexp');
 
 describe("GenerateSample", function () {
@@ -31,7 +34,11 @@ describe("GenerateSample", function () {
     });
 
     test('([{id}], 10) => [{id}].length=10 ', () => {
-        let sample = GenerateSample([{ key: "id", type: (index) => { return index; }}], 10);
+        let sample = GenerateSample([{
+            key: "id", type: (index) => {
+                return index;
+            }
+        }], 10);
         expect(sample.length).toBe(10);
         expect(sample[0]).not.toBeNull();
         expect(Object.keys(sample[0]).length).toBe(1);
@@ -43,7 +50,11 @@ describe("GenerateSample", function () {
     });
 
     test('([{digit}], 10) => [{digit}].length=10 ', () => {
-        let sample = GenerateSample([{ key: "digit", type: (index) => { return Math.floor(Math.random() * 10); }}], 10);
+        let sample = GenerateSample([{
+            key: "digit", type: (index) => {
+                return Math.floor(Math.random() * 10);
+            }
+        }], 10);
         expect(sample.length).toBe(10);
         expect(sample[0]).not.toBeNull();
         expect(Object.keys(sample[0]).length).toBe(1);
@@ -77,12 +88,12 @@ describe("RowNumber", function () {
     test('([{id:RowNumber}], 10) throws "Type of entity can\'t be a function."', () => {
         expect(() => {
             let sample = GenerateSample([
-                { key: "id", type: (index) => RowNumber }
+                {key: "id", type: (index) => RowNumber}
             ], 10);
         }).toThrow();
         expect(() => {
             let sample = GenerateSample([
-                { key: "id", type: (index) => RowNumber }
+                {key: "id", type: (index) => RowNumber}
             ], 10);
         }).toThrowError(/^Type of entity can't be a function.$/);
     });
@@ -92,7 +103,10 @@ describe("RowNumber", function () {
         let sample = GenerateSample([
             {
                 key: "digit",
-                type: (index) => { fun(); return index; }
+                type: (index) => {
+                    fun();
+                    return index;
+                }
             }
         ], 10);
         expect(fun).toHaveBeenCalledTimes(10);
@@ -100,7 +114,7 @@ describe("RowNumber", function () {
 
     test('([{id:RowNumber()}], 10) => [{id:Number}].length=10', () => {
         let sample = GenerateSample([
-            { key: "id", type: (index) => RowNumber(index) }
+            {key: "id", type: (index) => RowNumber(index)}
         ], 10);
         expect(sample.length).toBe(10);
         expect(sample[0]).not.toBeNull();
@@ -250,7 +264,7 @@ describe("regex", function () {
 
     test('([{value:RandomRegEx()}], 10) => [{value:Value}].length=10', () => {
         let sample = GenerateSample([
-            { key: "value", type: (index) => RandomRegEx(new RandExp(/hello+ (world|to you)/)) }
+            {key: "value", type: (index) => RandomRegEx(new RandExp(/hello+ (world|to you)/))}
         ], 10);
         expect(sample.length).toBe(10);
         expect(sample[0]).not.toBeNull();
@@ -264,4 +278,74 @@ describe("regex", function () {
 
 
     // new RandExp(/hello+ (world|to you)/).gen();
+})
+
+describe("email", function () {
+    test('RandomEmail is a function', () => {
+        expect(RandomEmail).not.toBeUndefined();
+        expect(RandomEmail).not.toBeNull();
+        expect(RandomEmail).toEqual(expect.any(Function));
+    });
+
+    test('([{value:RandomEmail()}], 10) => [{value:Email}].length=10', () => {
+        let sample = GenerateSample([
+            {key: "email", type: (index) => RandomEmail()}
+        ], 10);
+        expect(sample.length).toBe(10);
+        expect(sample[0]).not.toBeNull();
+        expect(Object.keys(sample[0]).length).toBe(1);
+        expect(sample[0].email).not.toBeNull();
+    });
+})
+
+describe("date", function () {
+    test('RandomDate is a function', () => {
+        expect(RandomDate).not.toBeUndefined();
+        expect(RandomDate).not.toBeNull();
+        expect(RandomDate).toEqual(expect.any(Function));
+    });
+
+    test('RandomDate is a function (start:!Date, _) throws', () => {
+        expect(() => {
+            let result = RandomDate('c', 1);
+        }).toThrowError(/^First Argument is Not An Date.$/);
+    });
+
+    test('RandomDate is a function (start:Date, end: !Date) throws', () => {
+        expect(() => {
+            let result = RandomDate(new Date(0), 'c');
+        }).toThrowError(/^Second Argument is Not An Date.$/);
+    });
+
+    test('RandomDate is a function (start:Date, end: Date) start > end throws', () => {
+        expect(() => {
+            let result = RandomDate(new Date(), new Date(0));
+        }).toThrowError(/^Second Argument is Greater Than First Argument.$/);
+    });
+
+    test('RandomDate is a function (start:Date, end: Date) start = end throws', () => {
+        expect(() => {
+            let result = RandomDate(new Date(0), new Date(0));
+        }).toThrowError(/^Second Argument is Greater Than First Argument.$/);
+    });
+
+    test('RandomDate is a function (start:Date, end: Date) => value:Number between (inclusive)start and end', () => {
+        let result = RandomDate(new Date(0), new Date());
+
+        expect(result).not.toBeUndefined();
+        expect(result).not.toBeNull();
+        expect(result).toEqual(expect.any(Date));
+        expect(result.getTime()).toBeGreaterThanOrEqual(new Date(0).getTime());
+        expect(result.getTime()).toBeLessThan(new Date().getTime());
+    });
+
+    test('([{value:RandomDate()}], 10) => [{value:Date}].length=10', () => {
+        let sample = GenerateSample([
+            {key: "value", type: (index) => RandomDate(new Date(0), new Date())}
+        ], 10);
+        expect(sample.length).toBe(10);
+        expect(sample[0]).not.toBeNull();
+        expect(Object.keys(sample[0]).length).toBe(1);
+        expect(sample[0].value).not.toBeNull();
+    });
 })
